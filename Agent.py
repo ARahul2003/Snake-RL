feeda06ea56bd2ca8b5d41354ba4af5f080df34c
Rahul A <rahul.a@techolution.com>
2023-08-21 00:51:29 +0530

    Implement the code for the neural network, and the code for tracking the progress

--- Agent.py
+++ Agent.py
@@ -2,9 +2,9 @@ import torch
 import random
 import numpy as np
 from collections import deque
-from game import SnakeGameAI, Direction, Point
+from snake_ai import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
-from helper import plot
+from track_progress import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
